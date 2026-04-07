7db7d6342fe0d8caae1c97fa92d1f252c3f1ebd4
Miradi <mengimiradit@gmail.com>
2026-04-07 03:06:58 +0200

    preparation deployement

--- staticfiles/js/style.js
+++ staticfiles/js/style.js
@@ -1,1 +1,20 @@
-
+function AfficherMenu(){
+    const menu=document.querySelector('#menu_bar')
+    const closed_bar=document.querySelector('#closed_bar')
+    const nav=document.querySelector('nav ul')
+     const rgba=document.querySelector('.rgba')
+    menu.addEventListener('click',()=>{
+        nav.classList.add('activemenu')
+        rgba.classList.add('rgbaactive')
+    })
+    closed_bar.addEventListener('click',()=>{
+        nav.classList.remove('activemenu')
+        rgba.classList.remove('rgbaactive')
+    })
+    rgba.addEventListener('click',()=>{
+        nav.classList.remove('activemenu')
+        rgba.classList.remove('rgbaactive')
+    })
+    
+}
+AfficherMenu()
