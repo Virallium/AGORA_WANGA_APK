209ca8eb76c1a0a9183acf15e940bf1e3cb92662
Virallium <mengimiradit@gmail.com>
2026-07-10 22:16:23 +0200

    push de la version où j'ai modifié le design de l'apk

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config,Csv
-import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -25,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-15ay6xw30q*^(nqweomf=$9u)t=7y_-ixt_@62)kad5uwc4c9-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
