4a3c63191a329040c084526b2e3814df962935eb
Virallium <mengimiradit@gmail.com>
2026-07-11 00:25:48 +0200

    modification du design

--- static/js/animate.js
+++ static/js/animate.js
@@ -1,8 +1,6 @@
-window.addEventListener('load', ()=>{
-    const loading_container=document.querySelector('.loading')
-    const loading_circle=document.querySelector('.loading .circle ')
-    const loading_circle_img=document.querySelector('.loading .circle img')
-    loading_container.classList.add('removeloading')
-    loading_circle.classList.add('removeloadingcircle')
-    loading_circle_img.classList.add('removeloadingimg')
-})
+window.addEventListener('load', () => {
+    const loadingContainer = document.querySelector('.loading');
+    if (loadingContainer) {
+        loadingContainer.classList.add('hidden');
+    }
+});
